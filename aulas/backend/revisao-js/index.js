fc3f687e79af9847917c4b8ed6dcc4e82381f926
ffernanda85 <f.fernanda85@gmail.com>
2023-05-26 23:20:31 -0300

    criados as funções de treinamento

---
+++ aulas/backend/revisao-js/index.js
@@ -0,0 +1,13 @@
+//Nesse caso o operador de comparação leva em consideração a ordem alfabetica para organizar 
+let v1 = 2
+
+/* function increment() {
+    //const f = v1++
+    const g = ++v1
+
+    console.log(g)
+} */
+
+//increment()
+console.log(v1++)
+console.log(v1)
