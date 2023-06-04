9cc42b5dbefc53d09e6ecd51ce52b236974645b3
ffernanda85 <f.fernanda85@gmail.com>
2023-06-03 23:44:52 -0300

    Criados arquivos template aula api

---
+++ aulas/backend/apis-e-express/src/database.ts
@@ -0,0 +1,22 @@
+import { COURSE_STACK, TCourse } from "./types"
+
+export const courses: TCourse[] = [
+    {
+        id: "c001",
+        name: "React",
+        lessons: 12,
+        stack: COURSE_STACK.FRONT
+    },
+    {
+        id: "c002",
+        name: "Styled Components",
+        lessons: 4,
+        stack: COURSE_STACK.FRONT
+    },
+    {
+        id: "c003",
+        name: "Express",
+        lessons: 5,
+        stack: COURSE_STACK.BACK
+    }
+]
---
+++ aulas/backend/apis-e-express/src/types.ts
@@ -0,0 +1,11 @@
+export enum COURSE_STACK {
+    FRONT = "Front-end",
+    BACK = "Back-end"
+}
+
+export type TCourse = {
+    id: string,
+    name: string,
+    lessons: number,
+    stack: COURSE_STACK
+}
