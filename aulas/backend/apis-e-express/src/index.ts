2e359a41cb0bc2600538173d845a44ea7a04c1a8
ffernanda85 <f.fernanda85@gmail.com>
2023-06-04 04:12:26 -0300

    Concluída primeira prática guiada da aula apis e express

--- aulas/backend/apis-e-express/src/index.ts
+++ aulas/backend/apis-e-express/src/index.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { courses } from './database'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
+//aponta para a porta 3003 para rodar o servidor
 app.listen(3003, () => {
     console.log("Servidor rodando na porta 3003")
 })
@@ -13,3 +15,8 @@ app.listen(3003, () => {
 app.get('/ping', (req: Request, res: Response) => {
     res.send('Pong!')
 })
+
+//pegar todos os cursos
+app.get("/courses", (req: Request, res: Response) => {
+    res.status(200).send(courses)
+});
