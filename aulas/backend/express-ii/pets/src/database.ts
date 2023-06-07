257f4cdf995caff7a8f1ccbf384637b88fb6c7c1
ffernanda85 <f.fernanda85@gmail.com>
2023-06-06 23:06:38 -0300

    criação arquivos assincrono express

---
+++ aulas/backend/express-ii/pets/src/database.ts
@@ -0,0 +1,28 @@
+import { PET_SIZE, TPet } from "./types";
+
+export const pets: TPet[] = [
+    {
+		id: "p001",
+		name: "Thor",
+		age: 5,
+		size: PET_SIZE.MEDIUM
+	},
+	{
+		id: "p002",
+		name: "Lili",
+		age: 10,
+		size: PET_SIZE.SMALL
+	},
+	{
+		id: "p003",
+		name: "Pingo",
+		age: 3,
+		size: PET_SIZE.LARGE
+	},
+	{
+		id: "p004",
+		name: "Luna",
+		age: 7,
+		size: PET_SIZE.MEDIUM
+	}
+]
---
+++ aulas/backend/express-ii/pets/src/index.ts
@@ -0,0 +1,59 @@
+import express, { Response, Request } from 'express';
+import cors from 'cors';
+import { pets } from './database';
+import { TPet } from './types';
+
+//criação do servidor express
+const app = express();
+
+//configurando respostas em JSON
+app.use(express.json());
+
+//habilitando o cors para rodar externamente
+app.use(cors());
+
+//startando o servidor na porta 3003
+app.listen(3003, () => {
+    console.log("servidor rodando na porta 3003");
+})
+
+//getAllPets
+app.get("/pets", (req: Request, res: Response) => {
+    res.status(200).send(pets)
+})
+
+//searchPetByName
+app.get("/pets/search", (req: Request, res: Response) => {
+    const name = req.query.name
+
+    const result = pets.filter(pet => {
+        return pet.name.toLowerCase().includes(name.toString().toLowerCase())
+    })
+    res.status(200).send(result)
+})
+
+//createNewPet
+app.post("/pets", (req: Request, res: Response) => {
+    const { id, name, age, size } = req.body
+    
+    const newPet: TPet = {
+        id,
+        name,
+        age,
+        size
+    }
+
+    pets.push(newPet)
+
+    pets.sort((a, b) => {
+        if (a.id < b.id) {
+            return -1
+        } else if (a.id > b.id) {
+            return 1
+        } else {
+            return 0
+        }
+    })
+
+    res.status(201).send("Cadastro de Pet Realizado com Sucesso!")
+})
