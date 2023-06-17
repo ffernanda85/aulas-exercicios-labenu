4f3218f5fc72908c52a2834d16df5c91b3790598
ffernanda85 <f.fernanda85@gmail.com>
2023-06-17 18:03:13 -0300

    Modified: tabela users editada

--- aulas/backend/introducao-ao-sql/teste-sqlite/test.sql
+++ aulas/backend/introducao-ao-sql/teste-sqlite/test.sql
@@ -12,5 +12,13 @@ PRAGMA table_info('users');
 INSERT INTO users (id, name, email, password)
 VALUES 
     (2, "Fernanda", "fernanda@gmail.com", "fernanda123"),
-    (3, "Izabela", "izabela@gmail.com", "izabela123")
+    (3, "Izabela", "izabela@gmail.com", "izabela123");
 
+UPDATE users
+SET 
+    email = "iza@gmail.com",
+    password = "iza123"
+WHERE id = 3;
+
+DELETE FROM users
+WHERE id = 2;
