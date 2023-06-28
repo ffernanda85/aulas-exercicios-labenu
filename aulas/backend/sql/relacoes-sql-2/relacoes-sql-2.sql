e97de363c45bf4f6062405a203fecc3abb41d766
ffernanda85 <f.fernanda85@gmail.com>
2023-06-28 15:05:01 -0300

    Develop: criando as queries de selects

--- aulas/backend/sql/relacoes-sql-2/relacoes-sql-2.sql
+++ aulas/backend/sql/relacoes-sql-2/relacoes-sql-2.sql
@@ -28,3 +28,48 @@ VALUES
     ("u001", "u003"),
     ("u002", "u001");
 
+-- Mostrando apenas a interseção, ou seja, apenas usuários que seguem alguém
+SELECT * FROM users
+INNER JOIN follows
+ON follows.follower_id = users.id;
+
+-- Mostrando todos os usuários, mesmo os que não seguem ninguém
+SELECT * FROM users
+LEFT JOIN follows
+ON follows.follower_id = users.id;
+
+--Mostrando todos os usuários, mesmo os que não seguem ninguém, porém ao invés de mostrar tudo seleciona apenas dados especificos
+SELECT 
+    users.id AS user_id,
+    name,
+    email AS user_email,
+    followed_id
+FROM users
+LEFT JOIN follows
+ON follows.follower_id = users.id;
+
+--Fazendo a junção de dois LEFT JOIN mostramos também os nomes dos usuários que são seguidos. Lembrando de tratar as ambiguidades
+SELECT 
+    users.id AS userId,
+    users.name AS userName,
+    users.created_at,
+    follows.followed_id AS followerId,
+    usersFollowed.name AS followedName
+FROM users
+LEFT JOIN follows
+ON follows.follower_id = users.id
+LEFT JOIN users AS usersFollowed
+ON follows.followed_id = usersFollowed.id;
+
+
+-- Mostrando apenas dados referentes ao user001
+SELECT 
+    users.id AS user_id,
+    name,
+    email AS user_email,
+    followed_id
+FROM users
+INNER JOIN follows
+ON follows.follower_id = users.id
+WHERE users.id = "u001";
+
