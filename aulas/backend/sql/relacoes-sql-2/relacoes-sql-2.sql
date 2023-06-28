c3a8e0cb4848990e57b8c987bf79fdafde6157c8
ffernanda85 <f.fernanda85@gmail.com>
2023-06-28 13:33:43 -0300

    Develop: criada e populada tabela de relações follows

--- aulas/backend/sql/relacoes-sql-2/relacoes-sql-2.sql
+++ aulas/backend/sql/relacoes-sql-2/relacoes-sql-2.sql
@@ -4,13 +4,27 @@ CREATE TABLE users (
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    created_at TEXT NOT NULL
+    created_at TEXT DEFAULT (DATETIME('now', 'localtime')) NOT NULL
 );
 
-INSERT INTO users (id, name, email, password, created_at)
+INSERT INTO users (id, name, email, password)
 VALUES
-    ("u001", "Flávia Fernanda", "flavia@email", "flavia123", DATETIME('now', 'localtime')),
-    ("u002", "Izabela Alves", "izabela@email", "izabela123", DATETIME('now', 'localtime')),
-    ("u003", "Fernanda", "fernanda@email", "fernanda123", DATETIME('now', 'localtime'));
+    ("u001", "Flávia Fernanda", "flavia@email", "flavia123"),
+    ("u002", "Izabela Alves", "izabela@email", "izabela123"),
+    ("u003", "Fernanda", "fernanda@email", "fernanda123");
 
 SELECT * FROM users;
+
+CREATE TABLE follows (
+    follower_id TEXT NOT NULL,
+    followed_id TEXT NOT NULL,
+    FOREIGN KEY (follower_id) REFERENCES users(id),
+    FOREIGN KEY (followed_id) REFERENCES users(id)
+);
+
+INSERT INTO follows (follower_id, followed_id)
+VALUES
+    ("u001", "u002"),
+    ("u001", "u003"),
+    ("u002", "u001");
+
