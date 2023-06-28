a78e975889ed0e78c50089a066ac99785ae16c8a
ffernanda85 <f.fernanda85@gmail.com>
2023-06-28 17:14:56 -0300

    Develop: criadas e populadas as tabelas banks, clients e clients_banks

--- aulas/backend/sql/relacoes-sql-2/relacoes-sql-2.sql
+++ aulas/backend/sql/relacoes-sql-2/relacoes-sql-2.sql
@@ -73,3 +73,84 @@ INNER JOIN follows
 ON follows.follower_id = users.id
 WHERE users.id = "u001";
 
+-- ================== FIXAÇÃO =======================
+
+/* 
+    Implementando uma relação de m:n, que consiste na relação de bancos e clientes. Onde temos bancos com vários clientes e clientes com conta em vários bancos. 
+*/
+
+CREATE TABLE clients (
+    id TEXT PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    cpf TEXT UNIQUE NOT NULL
+);
+
+DROP  TABLE banks;
+
+INSERT INTO clients (id, name, cpf)
+VALUES
+    ("c001", "Flávia Santos", "555.555.555-55"),
+    ("c002", "Flávia Fernanda", "222.222.222-22"),
+    ("c003", "Izabela Santos", "333.333.333-33"),
+    ("c004", "Flávia Alves", "111.111.111-11"),
+    ("c005", "Izabela Alves", "444.444.444-44");
+
+CREATE TABLE banks (
+    id TEXT PRIMARY KEY NOT NULL,
+    name TEXT UNIQUE NOT NULL
+);
+
+INSERT INTO banks (id, name)
+VALUES
+    ("b001", "Itaú"),
+    ("b002", "Bradesco"),
+    ("b003", "Santander"),
+    ("b004", "Nubank"),
+    ("b005", "Inter");
+
+CREATE TABLE clients_banks (
+    client_id TEXT NOT NULL,
+    banks_id TEXT NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES clients(id),
+    FOREIGN KEY (banks_id) REFERENCES banks(id)
+);
+
+INSERT INTO clients_banks (client_id, banks_id)
+VALUES
+    ("c001", "b001"),
+    ("c001", "b002"),
+    ("c001", "b004"),
+    ("c005", "b003"),
+    ("c005", "b004");
+
+SELECT * FROM clients;
+
+SELECT * FROM banks;
+   
+SELECT * FROM clients_banks;
+
+--Mostrando todos os clientes, inclusive os que não estão vinculados a nenhum dos bancos
+SELECT
+    clients.id AS clientId,
+    clients.name AS clientName,
+    cpf,
+    banks.id AS bankId,
+    banks.name AS bankName
+FROM clients
+LEFT JOIN clients_banks
+ON clients_banks.client_id = clients.id
+LEFT JOIN banks
+ON clients_banks.banks_id = banks.id;
+
+--Mostra todos os clientes que estão vinculados a algum banco
+SELECT
+    clients.id AS clientId,
+    clients.name AS clientName,
+    cpf,
+    banks.id AS bankId,
+    banks.name AS bankName
+FROM banks
+INNER JOIN clients_banks
+ON banks.id = clients_banks.banks_id
+INNER JOIN clients
+ON clients.id = clients_banks.client_id;
