2bc4ff1819b278ced1fbb6aaf6a4964bbd790693
ffernanda85 <f.fernanda85@gmail.com>
2023-07-21 20:54:19 -0300

    Develop: criação de pastas e configurações de conexão

---
+++ aulas/backend/POO/poo-3/poo-III-template-main/src/database/poo.sql
@@ -0,0 +1,26 @@
+-- Active: 1675168195024@@127.0.0.1@3306
+CREATE TABLE users (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    created_at TEXT DEFAULT (DATETIME()) NOT NULL
+);
+
+CREATE TABLE accounts (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    owner_id TEXT NOT NULL,
+    balance REAL DEFAULT (0) NOT NULL,
+    created_at TEXT DEFAULT (DATETIME()) NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES users (id)
+);
+
+INSERT INTO users (id, name, email, password)
+VALUES
+	("u001", "Fulano", "fulano@email.com", "fulano123"),
+	("u002", "Beltrana", "beltrana@email.com", "beltrana00");
+
+INSERT INTO accounts (id, owner_id)
+VALUES
+	("a001", "u001"),
+	("a002", "u002");
