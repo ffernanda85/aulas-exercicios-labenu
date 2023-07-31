7f7cc5bdf43574c210dce497ac019deaf9e52a91
ffernanda85 <f.fernanda85@gmail.com>
2023-07-31 14:01:36 -0300

    create class badrequesterror

--- aulas/backend/arquitetura/ARQUITETURA_2/arquitetura-II-template-main/src/index.ts
+++ aulas/backend/arquitetura/ARQUITETURA_2/arquitetura-II-template-main/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
-import { UserController } from './controller/UserController'
-import { AccountController } from './controller/AccountController'
+import { userRouter } from './router/userRouter'
+import { accountRouter } from './router/accountRouter'
 
 const app = express()
 
@@ -12,13 +12,5 @@ app.listen(3003, () => {
     console.log(`Servidor rodando na porta ${3003}`)
 })
 
-const userController = new UserController()
-const accountController = new AccountController()
-
-app.get("/users", userController.getUsers)
-app.post("/users", userController.createUser)
-
-app.get("/accounts", accountController.getAccounts)
-app.get("/accounts/:id/balance", accountController.getAccountBalance)
-app.post("/accounts", accountController.createAccount)
-app.put("/accounts/:id/balance", accountController.editAccountBalance)
+app.use("/users", userRouter)
+app.use("/accounts", accountRouter)
---
+++ aulas/backend/arquitetura/ARQUITETURA_2/arquitetura-II-template-main/src/router/userRouter.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+import { UserController } from '../controller/UserController'
+
+export const userRouter = express.Router()
+const userController = new UserController()
+
+userRouter.get("/", userController.getUsers)
+userRouter.post("/", userController.createUser)
