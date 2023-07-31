973f6d234cef87596180858e34b6576632bc48f0
ffernanda85 <f.fernanda85@gmail.com>
2023-07-31 13:19:31 -0300

    Develop: create connection BD

--- aulas/backend/arquitetura/ARQUITETURA_2/arquitetura-II-template-main/src/database/arquitetura.sql
+++ aulas/backend/arquitetura/ARQUITETURA_2/arquitetura-II-template-main/src/database/arquitetura.sql
@@ -1,4 +1,4 @@
--- Active: 1675359864041@@127.0.0.1@3306
+-- Active: 1687367064663@@127.0.0.1@3306
 CREATE TABLE users (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL,
@@ -17,8 +17,10 @@ CREATE TABLE accounts (
 
 INSERT INTO users (id, name, email, password)
 VALUES
-	("u001", "Fulano", "fulano@email.com", "fulano123"),
-	("u002", "Beltrana", "beltrana@email.com", "beltrana00");
+	("u001", "Silvio Brás", "silvio@email.com", "sil123"),
+	("u002", "Fábio Melo", "binho@email.com", "binho123"),
+    ("u003", "Helaine Maria", "helaine@email.com", "123"),
+    ("u004", "Samuel Cabral", "samu@email.com", "samu123");
 
 INSERT INTO accounts (id, owner_id)
 VALUES
