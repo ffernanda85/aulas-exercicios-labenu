d253ba649b87cf58b38ed98b1b484e15e2b22346
ffernanda85 <f.fernanda85@gmail.com>
2023-08-28 21:11:47 -0300

    completed class material

--- aulas/fullstack/TESTS-BACKEND-I/testes-backend-I-template-main/tests/pratica2.test.ts
+++ aulas/fullstack/TESTS-BACKEND-I/testes-backend-I-template-main/tests/pratica2.test.ts
@@ -3,16 +3,9 @@ import { pratica2 } from "../src/pratica2"
 
 describe("Testing Practice 2", () => {
     
-    test("testing insert type is number", () => { //verifica o type de entrada, se for diferente de number ele retorna null
-        const result = pratica2(" dl´l´dlasd" as any)
-        //expect(result).toBeNull()// verifica se é null
-        //expect(result).toBe(null)
-        expect(result).toEqual(null)
+    test("retorna true para números pares e inteiros", () => { 
+        expect(pratica2(10)).toBe(true)
+        expect(pratica2(11)).toBe(false)
+        
     })
-
-    test("testing if input data is even", () => { //verificando paridade, caso retorne false dará erro, informando que o teste não passou na questão de paridade da função chamada dentro do result
-        const result = pratica2(10)
-        expect(result).toBe(true)
-    })
-    
 })
