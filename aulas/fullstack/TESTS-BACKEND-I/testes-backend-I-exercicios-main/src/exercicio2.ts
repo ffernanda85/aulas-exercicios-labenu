174852737ee1bee486834f64a88d6b677a52e104
ffernanda85 <f.fernanda85@gmail.com>
2023-08-29 12:07:38 -0300

    feat: criada função exercício 2

---
+++ aulas/fullstack/TESTS-BACKEND-I/testes-backend-I-exercicios-main/src/exercicio2.ts
@@ -0,0 +1,7 @@
+/* Crie uma função que recebe uma data no formato "aaaa/mm/dd" e retorna outra string no formato "dd/mm/aaaa".
+Exemplo: a entrada "2022/09/26" gera a saída "26/09/2022"
+Crie pelo menos um teste que valida sua implementação. */
+
+export const exercicio2 = () => {
+    
+}
